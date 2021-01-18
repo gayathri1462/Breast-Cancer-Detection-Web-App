32f0353ceffcf43201f4a244fc0fc1ffae88fcc0
Gayathri Devi Nagalapuram <gayathri1462@gmail.com>
2021-01-18 19:49:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+  input_features = [int(x) for x in request.form.values()]
+  features_value = [np.array(input_features)]
+
+  features_name = ['clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
+       'marginal_adhesion', 'single_epithelial_size', 'bare_nuclei',
+       'bland_chromatin', 'normal_nucleoli', 'mitoses']
+
+  df = pd.DataFrame(features_value, columns=features_name)
+  output = model.predict(df)
+
+  if output == 4:
+      res_val = "Breast cancer"
+  else:
+      res_val = "no Breast cancer"
+
+
+  return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
+
+if __name__ == "__main__":
+  app.run()
